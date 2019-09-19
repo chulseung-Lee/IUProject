a65c456ce04f48cc21b9ed2cb3035281e79b4539
zython-repo <dydgur316@gmail.com>
2019-09-19 21:35:22 +0900

    spring boot

---
+++ IULover/src/main/java/com/example/IULover/IuLoverApplication.java
@@ -0,0 +1,13 @@
+package com.example.IULover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuLoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuLoverApplication.class, args);
+	}
+
+}
